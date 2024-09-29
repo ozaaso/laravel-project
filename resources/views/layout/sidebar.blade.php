07d91962d4ed99abe471c731f76ee2b80d128cc9
ozaaso <ozaaso@gmail.com>
2024-09-29 11:53:36 +0700

    mantab

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -2,16 +2,16 @@
     <div class="position-sticky pt-3">
       <ul class="nav flex-column">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="{{ url('/dashboard') }}">
-            <span data-feather="home"></span>
-            Dashboard
-          </a>
+            <a class="nav-link {{ Request::is('dashboard') ? 'active' : '' }}" aria-current="page" href="{{ url('/dashboard') }}">
+                <span data-feather="home"></span>
+                Dashboard
+            </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="{{ url('/doctor') }}">
-            <span data-feather="file"></span>
-            Doctors
-          </a>
+            <a class="nav-link {{ Request::is('doctor*') ? 'active' : '' }}" href="{{ url('/doctor') }}">
+                <span data-feather="file"></span>
+                Doctors
+            </a>
         </li>
 
       </ul>
